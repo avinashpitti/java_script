0cfa296ecdb032c387d0057d2eac0fb545520661
avinashpitti <avinash87129@gmail.com>
2024-12-20 14:33:52 +0000

    my first commit

---
+++ avi.py
@@ -0,0 +1,7 @@
+a=int(input("enter the side of a square:"))
+print(a*a)
+
+a=float(input("Enter the First Number:"))3
+b=float(input("Enter the Second Number:"))
+print((a+b)/2)
+print(len("hello world"6))
