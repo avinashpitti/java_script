84183da1cdc1fddcc9527b9137142312d2afba49
avinashpitti <avinash87129@gmail.com>
2024-12-20 20:09:47 +0530

    Update avi.py

--- avi.py
+++ avi.py
@@ -1,7 +1,6 @@
 a=int(input("enter the side of a square:"))
 print(a*a)
 
-a=float(input("Enter the First Number:"))3
+a=float(input("Enter the First Number:"))
 b=float(input("Enter the Second Number:"))
 print((a+b)/2)
-print(len("hello world"6))
